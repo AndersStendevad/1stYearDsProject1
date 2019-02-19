85410565afa5a2e3e47a6144daf8f534a18745c4
Neil Kim Nielsen <neni@ageras.com>
2019-02-19 20:11:27 +0100

    Instagram scraper (working!)

---
+++ instagram_scraper.py
@@ -0,0 +1,32 @@
+from InstagramAPI import InstagramAPI
+import time
+from extensions import logger
+
+logger = logger.get_logger()
+
+def locationSearch(location, hits):
+    maxid = ''
+    for i in range(hits):
+        API.getHashtagFeed(location, maxid)
+        data = API.LastJson
+        try:
+            log.write(data.get('items')[0].get('caption').get('text'))
+            log.write('\n \n')
+            maxid = data.get('next_max_id')
+        except IndexError:
+            logger.info(f'error on fetch')
+            pass
+        time.sleep(1)
+
+API = InstagramAPI("neil_ds_guy", "mKh80994QCuY")
+API.login()
+
+size = 1000
+
+with open('instagram_locations.txt', 'r') as loc:
+    for line in loc:
+        location = line.strip()
+        logger.info(f'writing data to {location}')
+        log = open(location+".txt", "w")
+        locationSearch(location, 1)
+        log.close()
