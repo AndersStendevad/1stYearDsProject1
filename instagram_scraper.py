8cb6a1cdd1712b9e16cdebb88719d4906c85ed3d
Neil Kim Nielsen <neni@ageras.com>
2019-02-21 08:59:21 +0100

    raw instagram data

--- instagram_scraper.py
+++ instagram_scraper.py
@@ -10,23 +10,28 @@ def locationSearch(location, hits):
         API.getHashtagFeed(location, maxid)
         data = API.LastJson
         try:
-            log.write(data.get('items')[0].get('caption').get('text'))
+            for pic in data.get('items'):
+                if pic.get('caption'):
+                    log.write(pic.get('caption').get('text'))
+            #log.write(data.get('items')[0].get('caption').get('text'))
             log.write('\n \n')
             maxid = data.get('next_max_id')
-        except IndexError:
+        except Exception:
             logger.info(f'error on fetch')
+            logger.info(data)
+            break
             pass
         time.sleep(1)
 
 API = InstagramAPI("neil_ds_guy", "mKh80994QCuY")
 API.login()
 
-size = 1000
+size = 100
 
 with open('instagram_locations.txt', 'r') as loc:
     for line in loc:
         location = line.strip()
         logger.info(f'writing data to {location}')
         log = open(location+".txt", "w")
-        locationSearch(location, 1)
+        locationSearch(location, size)
         log.close()
