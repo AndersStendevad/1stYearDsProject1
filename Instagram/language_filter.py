c53511fc5e2da657f1836c3209ddffc2194cd45b
Sebestyen Nagy <sena@itu.dk>
2019-02-26 16:32:40 +0100

    language filter for instagram posts

---
+++ Instagram/language_filter.py
@@ -0,0 +1,22 @@
+from langdetect import detect
+
+def language_filter(infile, outfile, language):
+    inf = open(infile)
+    outf = open(outfile, "w")
+    for line in inf:
+        newline = line.strip().split("\n \n")
+        for i in newline:
+            try:    
+                if detect(i) == language and i != "":
+                    outf.write(i)
+            except Exception:
+                pass
+    inf.close()
+    outf.close()
+
+inlist = ["amager.txt", "kbhk.txt", "nørrebro.txt", "østerbro.txt", "torvehallerne.txt", "valby.txt", "vesterbro.txt"]
+#outlist = ["amager_lang-DA.txt", "kbhk_lang-DA.txt", "nørrebro_lang-DA.txt", "østerbro_lang-DA.txt", "torvehallerne_lang-DA.txt", "valby_lang-DA.txt", "vesterbro_lang-DA.txt"]
+
+for i in inlist:
+    j = i.split('.')
+    language_filter(i, j[0] + "_lang-DA.txt", "da")
