a9462058ec55a5bb89a972e5c28d363b8ee1eaf7
DovileDo <djuodelyte@gmail.com>
2019-03-05 16:24:13 +0100

    Cleaned streets and shops from vectorizer file

--- EDA/DataToVectoriserToPickle.py
+++ EDA/DataToVectoriserToPickle.py
@@ -3,10 +3,11 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.pipeline import FeatureUnion
 import pickle
 import os
+import re
 
 def main():
-    corpus = readFilesToList()
-    Sklearn(corpus)
+	corpus = readFilesToList()
+	Sklearn(corpus)
 
 ###
 
@@ -22,16 +23,16 @@ def readFilesToList():
     return corpus
 
 def Sklearn(corpus):
-    vectorizer = TfidfVectorizer(
-    lowercase=True, preprocessor=None,
-    tokenizer=None, stop_words=None,
-    ngram_range=(1, 1),max_df=None,
-    vocabulary=None,
-    )
-    X = vectorizer.fit_transform(corpus)
+	streets = re.findall(r"(\b\w*gade\w*\b)", corpus[0])
+	shops = re.findall(r"(\b\w*shop\w*\b)", corpus[0])
+	places = ['amagerbrogade', 'remix5', 'remix', 'torsdage','fredag','mandag','onsdage','korsgade','bragesgade','valhalsgade', 'kapelvej','dkårhusgade','dkøsterbrogade','dkblegdamsvej','øårhusgade','dkjagtvej','dklyngbyvej','dkserridslevvej','dksionsgade', 'telefon', 'bellmansgade', 'tirsdag','onsdag','amagers', 'nordrefrihavnsgade', 'viborggade', 'nordhavn', 'trianglen', 'østerbros', 'lyngbyvej', 'torvehallernes', 'frihavnsgade', 'strandboulevarden', 'nyhavn', 'østebros', 'nørrebrogade', 'nørrebros', 'nytorv', 'kastrup', 'copenhagen', 'amager', 'amagerstrand', 'nyamagerbrogade', 'københavn', 'amagerbro', 'sundby', 'tårnby', 'kbh', 'kbhk', 'reffen', 'reffencph', 'nørrebro', 'ravnsborggade', 'nordvest', 'holbæk', 'christianshavn', 'nørrebronx', 'frederiksberg', 'valby', 'hvidovre', 'torvehallerne', 'torvehallernekbh', 'elledanmark', 'valby', 'vesterbro', 'vesterbrogade', 'vesterbronx', 'istedgade', 'saxogade', 'istedgade110', 'aarhus', 'odense', 'aalborg', 'hellerup', 'østerbro', 'cph', 'blågårdsgade', 'cphk', 'upcopenhagen', 'kl', '02', '15', '18', '2010', 'lyngby', 'oslo', 'norge', 'stellaalice', 'østerbrogade', 'odensegade', 'århusgade', 'cm', 'str', 'kbhv', 'norway', 'vesterbros', 'hasserisgade', 'prinsensgade', 'kirkegårdsgade', 'prinsensgade', 'kødbyen', 'langgade', 'valbys', 'værkstedvej', 'skolegade', 'sydhavnen', 'torvehaller', 'torvehallen', 'torvehal', 'valdemarsgade', 'vesterbroere', 'vesterbroerne', 'jernbanevej', 'kulbanevej', 'voervadsbro', 'østerbroere', 'vestergård', 'svanemøllen', 'ryparken' ]
+	streets.extend(places)
+	streets.extend(shops)
+	vectorizer = TfidfVectorizer(lowercase=True, preprocessor=None, tokenizer=None, analyzer="word" ,stop_words=set(streets), token_pattern=r"(?u)\b\w\w\w\w+\b",ngram_range=(1, 1),max_df=0.5, vocabulary=None)
+	X = vectorizer.fit_transform(corpus)
     #print(vectorizer.get_feature_names())
-    print(X.shape)
-    pickle.dump(sorted(vectorizer.vocabulary_.items()), open("vocabulary.pickle", "wb"))
-    pickle.dump(X, open("tfidf.pickle", "wb"))
+	print(X.shape)
+	pickle.dump(sorted(vectorizer.vocabulary_.items()), open("vocabulary.pickle", "wb"))
+	pickle.dump(X, open("tfidf.pickle", "wb"))
 
 main()
